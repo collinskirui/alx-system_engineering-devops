c554ad13a6fe2a26dbe7d5f155708f50ad34aaf1
collinskirui <collins.kirui@strathmore.edu>
2022-11-28 21:40:03 -0800

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Puppet file to fix bad `phpp` extensions to `php` 500 error fix
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
